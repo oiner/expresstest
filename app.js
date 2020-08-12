8feea0781070471b7a1e8b017fa247ab3e9eaeb3
Hunter Philips <hrphilips7@gmail.com>
2020-08-12 18:23:09 -0400

    other parts of tutorial

--- app.js
+++ app.js
@@ -3,8 +3,34 @@ var app = express();
 const port = 3000
 
 app.get("/", (req, res) =>{
-	res.send("Hi there!");
+	res.send("Hello World!");
 });
+
+app.get("/bye", (req, res) => {
+	res.send("Goodbye, Cruel World!");
+})
+
+app.get("/meow", (req, res) =>{
+	console.log("I am a Cat. Meow.")
+	res.send("meow");
+})
+
+
+
+app.get("/r/:Pagename" , (req, res) =>{
+	console.log(req.params.Pagename);	
+	var pageTitle = req.params.Pagename;
+		res.send("Welcome to the" + req.params.Pagename + "subreddit")})
+
+
+
+
+app.get("*" ,  function(req, res){
+	res.send("You are my shining star");
+})
+
+
+
 
 app.listen(port, ()=>{
 	console.log('example app listening at http://localhost:${port}')
